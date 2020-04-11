0fdac946c39ac6b4345f5bf8f864bc0fb69b7a87
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-12 01:10:01 +0300

    Add address tests.

--- tests/Elrond/AddressTests.cpp
+++ tests/Elrond/AddressTests.cpp
@@ -15,34 +15,29 @@ using namespace TW;
 using namespace TW::Elrond;
 
 TEST(ElrondAddress, Valid) {
-    ASSERT_TRUE(Address::isValid("__ADD_VALID_ADDRESS_HERE__"));
-
-    // TODO: Add more tests
+    ASSERT_TRUE(Address::isValid("erd10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr"));
+    ASSERT_TRUE(Address::isValid("erd1wcfp4v3c7k5qzurue2xq2n60dq362kj56ccu9qz4pgd95vm09gaqsmh2h9"));
 }
 
 TEST(ElrondAddress, Invalid) {
-    ASSERT_FALSE(Address::isValid("__ADD_INVALID_ADDRESS_HERE__"));
-
-    // TODO: Add more tests
+    ASSERT_FALSE(Address::isValid(""));
+    ASSERT_FALSE(Address::isValid("foo"));
+    ASSERT_FALSE(Address::isValid("10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr"));
 }
 
 TEST(ElrondAddress, FromPrivateKey) {
-    // TODO: Check public key type, finalize implementation
-
-    auto privateKey = PrivateKey(parse_hex("__PRIVATE_KEY_DATA__"));
+    auto privateKey = PrivateKey(parse_hex("6d893bc800f790261f814be252ee9bf51d6efc70e8c46dedc3f9357d06487b04"));
     auto address = Address(privateKey.getPublicKey(TWPublicKeyTypeED25519));
-    ASSERT_EQ(address.string(), "__ADD_RESULTING_ADDRESS_HERE__");
+    ASSERT_EQ(address.string(), "erd10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr");
 }
 
 TEST(ElrondAddress, FromPublicKey) {
-    // TODO: Check public key type, finalize implementation
-    
-    auto publicKey = PublicKey(parse_hex("__PUBLIC_KEY_DATS__"), TWPublicKeyTypeED25519);
+    auto publicKey = PublicKey(parse_hex("788a66f11d24e8a3da1dcc1aefdd2f7e92ea410a06f7b7de8a4b0d8139198118"), TWPublicKeyTypeED25519);
     auto address = Address(publicKey);
-    ASSERT_EQ(address.string(), "__ADD_RESULTING_ADDRESS_HERE__");
+    ASSERT_EQ(address.string(), "erd10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr");
 }
 
 TEST(ElrondAddress, FromString) {
-    auto address = Address("__ADD_VALID_ADDRESS_HERE__");
-    ASSERT_EQ(address.string(), "__ADD_SAME_VALID_ADDRESS_HERE__");
+    auto address = Address("erd10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr");
+    ASSERT_EQ(address.string(), "erd10z9xdugayn528ksaesdwlhf006fw5sg2qmmm0h52fvxczwgesyvq5pwemr");
 }
