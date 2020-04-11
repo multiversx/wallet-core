a061ddcfc6530d0492e0bdc37ad1b719e9b5241e
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-11 18:26:33 +0000

    Initial commit, add coin and generated files.

--- include/TrustWalletCore/TWCoinType.h
+++ include/TrustWalletCore/TWCoinType.h
@@ -79,6 +79,7 @@ enum TWCoinType {
     TWCoinTypeKusama = 434,
     TWCoinTypePolkadot = 354,
     TWCoinTypeFilecoin = 461,
+    TWCoinTypeElrond = 2003,
 };
 
 /// Returns the blockchain for a coin type.
