e30ab996ecdeb2e5d1d7be95c0842d377698ea36
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-16 19:39:39 +0000

    Kotlin tests.

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/elrond/TestElrondAddress.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/elrond/TestElrondAddress.kt
@@ -18,16 +18,33 @@ class TestElrondAddress {
         System.loadLibrary("TrustWalletCore")
     }
 
-    @Test
-    fun testAddress() {
-        // TODO: Check and finalize implementation
+    val aliceBech32 = "erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz"
+    var aliceSeedHex = "1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf"
+    var alicePubKeyHex = "fd691bb5e85d102687d81079dffce842d4dc328276d2d4c60d8fd1c3433c3293"
 
-        val key = PrivateKey("__PRIVATE_KEY_DATA__".toHexByteArray())
+    @Test
+    fun testAddressFromPrivateKey() {
+        val key = PrivateKey(aliceSeedHex.toHexByteArray())
         val pubkey = key.publicKeyEd25519
         val address = AnyAddress(pubkey, CoinType.ELROND)
-        val expected = AnyAddress("__EXPECTED_RESULT_ADDRESS__", CoinType.ELROND)
+        val expected = AnyAddress(aliceBech32, CoinType.ELROND)
 
-        assertEquals(pubkey.data().toHex(), "0x__EXPECTED_PUBKEY_DATA__")
+        assertEquals(pubkey.data().toHex(), alicePubKey)
         assertEquals(address.description(), expected.description())
+    }
+
+    @Test
+    fun testAddressFromPublicKey() {
+        val pubkey = PublicKey(alicePubKeyHex.toHexByteArray(), PublicKeyType.ED25519)
+        val address = AnyAddress(pubkey, CoinType.ELROND)
+
+        assertEquals(address.description(), aliceBech32)
+    }
+
+    @Test
+    fun testAddressFromString() {
+        val address = AnyAddress(aliceBech32, CoinType.ELROND)
+        
+        assertEquals(address.description(), aliceBech32)
     }
 }
--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/elrond/TestElrondSigner.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/elrond/TestElrondSigner.kt
@@ -22,24 +22,43 @@ class TestElrondSigner {
         System.loadLibrary("TrustWalletCore")
     }
 
+    val aliceBech32 = "erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz"
+    var aliceSeedHex = "1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf"
+    var alicePubKeyHex = "fd691bb5e85d102687d81079dffce842d4dc328276d2d4c60d8fd1c3433c3293"
+
+    val bobBech32 = "erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r"
+    var bobSeedHex = "e3a3a3d1ac40d42d8fd4c569a9749b65a1250dd3d10b6f4e438297662ea4850e"
+    var bobPubKeyHex = "c70cf50b238372fffaf7b7c5723b06b57859d424a2da621bcc1b2f317543aa36"
+
     @Test
     fun ElrondTransactionSigning() {
-        // TODO: Finalize implementation
+        val transaction = Elrond.TransactionMessage.newBuilder()
+            .setNonce(0)
+            .setValue("0")
+            .setSender(aliceBech32)
+            .setReceiver(bobBech32)
+            .setGasPrice(200000000000000)
+            .setGasLimit(500000000)
+            .setData("foo)
+            .build()
+        
+        var aliceKey = ByteString.copyFrom(PrivateKey(aliceSeedHex.toHexByteArray()).data())
 
-        //val transfer = Elrond.TransferMessage.newBuilder()
-        //    .setTo("...")
-        //    .setAmount(...)
-        //    ...
-        //    .build()
-        //val signingInput = Elrond.SigningInput.newBuilder()
-        //    ...
-        //    .build()
+        val signingInput = Elrond.SigningInput.newBuilder()
+            .setPrivateKey(aliceKey)
+            .setTransaction(transaction)
+            .build()
 
-        //val output: Elrond.SigningOutput = ElrondSigner.sign(signingInput)
+        val output: Elrond.SigningOutput = ElrondSigner.sign(signingInput)
 
-        //assertEquals(
-        //    "__EXPECTED_RESULT_DATA__",
-        //    Numeric.toHexString(output.encoded.toByteArray())
-        //)
+        assertEquals(
+            "b88ad2fe98a7316ea432a0a76c18cd87200fe75f27a8f053ea6532b40317dbec5136c5463aef132ae951b7e60d45d921caaa5903e70821dcda98f237d4ec4308",
+            Numeric.toHexString(output.signature.toByteArray())
+        )
+
+        // TODO:
+        // val sign = AnySigner.sign(signingInput.build(), ELROND, SigningOutput.parser())
+        // val signBytes = sign.signature
+        // assertEquals(signBytes, "")
     }
 }
