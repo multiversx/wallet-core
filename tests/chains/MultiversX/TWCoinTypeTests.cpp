2b6fe6f2f5fb34385f9d4e0afa697edd540d195f
andreibancioiu <andrei.bancioiu@gmail.com>
2023-01-27 16:28:45 +0200

    Rebranding.

--- tests/chains/MultiversX/TWCoinTypeTests.cpp
+++ tests/chains/MultiversX/TWCoinTypeTests.cpp
@@ -13,14 +13,14 @@
 #include <gtest/gtest.h>
 
 
-TEST(TWElrondCoinType, TWCoinType) {
-    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeElrond));
+TEST(TWMultiversXCoinType, TWCoinType) {
+    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeMultiversX));
     auto txId = WRAPS(TWStringCreateWithUTF8Bytes("163b46551a74626415074b626d2f37d3c78aef0f6ccb628db434ee65a35ea127"));
-    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeElrond, txId.get()));
+    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeMultiversX, txId.get()));
     auto accId = WRAPS(TWStringCreateWithUTF8Bytes("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"));
-    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeElrond, accId.get()));
-    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeElrond));
-    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeElrond));
+    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeMultiversX, accId.get()));
+    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeMultiversX));
+    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeMultiversX));
 
     ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeMultiversX), 18);
     ASSERT_EQ(TWBlockchainMultiversX, TWCoinTypeBlockchain(TWCoinTypeMultiversX));
@@ -29,6 +29,6 @@ TEST(TWElrondCoinType, TWCoinType) {
     assertStringsEqual(symbol, "eGLD");
     assertStringsEqual(txUrl, "https://explorer.multiversx.com/transactions/163b46551a74626415074b626d2f37d3c78aef0f6ccb628db434ee65a35ea127");
     assertStringsEqual(accUrl, "https://explorer.multiversx.com/accounts/erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th");
-    assertStringsEqual(id, "elrond");
+    assertStringsEqual(id, "multiversx");
     assertStringsEqual(name, "MultiversX");
 }
