11fd2e5439ee8c136ae6c1b7c1ccbec4ed6f7665
Lucian Savencu <lucian.savencu@icloud.com>
2023-04-24 18:29:53 +0300

    Updated Swift Package Manager specs

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.8/WalletCore.xcframework.zip",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.28/WalletCore.xcframework.zip",
             checksum: "ce7e45a8eea666dacc5cd9076f597e4f824c030299414c7fdd91e71b29aa09cc"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.8/SwiftProtobuf.xcframework.zip",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.28/SwiftProtobuf.xcframework.zip",
             checksum: "222a306b49d7733b9c5d790b07c0d8240f68f27315f1d127aa4ed743b1b11920"
         )
     ]
